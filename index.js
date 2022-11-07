f182337c96225effb3b4518b001a9b7fc36d6150
Yeamin0007 <yh.hossainsaaz@gmail.com>
2022-11-08 00:37:41 +0600

    server basic setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.Port || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.send('snap server is running')
+})
+
+app.listen(port, ()=>{
+    console.log(`snap server running on ${port}`);
+})
