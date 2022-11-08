da8543f015a10150633656969fbd545261e90e92
Yeamin0007 <yh.hossainsaaz@gmail.com>
2022-11-09 00:52:28 +0600

    load services data

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ async function run(){
             const services = await cursor.toArray();
             res.send(services)
         })
+        
+        app.get('/allservices', async(req,res)=>{
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
     }
     finally{}
 }
