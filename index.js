b2c3b2bcb1076bea434131eaf53aaea36bb708a6
Yeamin0007 <yh.hossainsaaz@gmail.com>
2022-11-10 18:26:20 +0600

    get id from review

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ async function run(){
             const services = await cursor.limit(3).toArray();
             res.send(services)
         })
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        })
+        
         
         app.get('/allservices', async(req,res)=>{
             const query = {}
@@ -69,6 +75,13 @@ async function run(){
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send(result);
+        })
+
+        app.get('/reviews/:id', async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await reviewCollection.findOne(query);
+            res.send(service);
         })
 
         
