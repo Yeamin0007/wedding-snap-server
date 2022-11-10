d9893d448e18602308ea3a6ebf8323558d2ae0b0
Yeamin0007 <yh.hossainsaaz@gmail.com>
2022-11-10 22:04:49 +0600

    add jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express()
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -14,10 +15,32 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        res.status(401).send({message: 'unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            res.status(401).send({message: 'unsuthorized access'})
+        }
+        req.decoded =decoded;
+        next();
+    })
+}
 async function run(){
     try{
         const serviceCollection = client.db('weddingSnap').collection('services');
         const reviewCollection = client.db('weddingSnap').collection('reviews');
+
+
+        app.post('/jwt', (req, res) =>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
+            res.send({token})
+        })
 
         app.get('/services', async(req,res)=>{
             const query = {}
@@ -47,7 +70,12 @@ async function run(){
         })
 
         // review sec
-        app.get('/reviews', async(req, res)=>{
+        app.get('/reviews', verifyJWT, async(req, res)=>{
+            const decoded = req.decoded;
+            console.log(decoded);
+            if(decoded.email !== req.query.email){
+                res.status(403).send({message: 'unauthorized access'})
+            }
             let query = {};
             if( req.query.email){
                 query = {
